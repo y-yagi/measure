0f022536813f926e18d9771bf73f69c43163f522
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-07-21 10:46:11 +0900

    add test

---
+++ main_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestMeasure(t *testing.T) {
+	out, err := new(bytes.Buffer), new(bytes.Buffer)
+
+	measure("testdata/test.zip", out, err)
+	expected := "testdata/test.zip: 8.5K\n"
+	if out.String() != expected {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+
+	out.Reset()
+	measure("https://github.com/y-yagi/rails_api_sample/archive/master.zip", out, err)
+	expected = "Redirectd to https://codeload.github.com/y-yagi/rails_api_sample/zip/master\nhttps://github.com/y-yagi/rails_api_sample/archive/master.zip: 28.7K\n"
+	if out.String() != expected {
+		t.Errorf("Expect out is %q, but %q", out.String(), expected)
+	}
+}
