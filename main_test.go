5ea30843833b5d422f007ac80c98da1f4f494550
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-04-09 17:47:03 +0900

    Adjust messages

--- main_test.go
+++ main_test.go
@@ -12,20 +12,20 @@ func TestMeasure(t *testing.T) {
 	measure("testdata/test.zip", retryCount, out, err)
 	expected := "testdata/test.zip: \x1b[32m8.5K\x1b[0m\n"
 	if out.String() != expected {
-		t.Fatalf("Expect out is %q, but %q", out.String(), expected)
+		t.Fatalf("Expected '%q', but got '%q'", expected, out.String())
 	}
 
 	out.Reset()
 	measure("https://raw.githubusercontent.com/y-yagi/rails_api_sample/master/Gemfile", retryCount, out, err)
 	expected = "https://raw.githubusercontent.com/y-yagi/rails_api_sample/master/Gemfile: \x1b[32m958B\x1b[0m\n"
 	if out.String() != expected {
-		t.Fatalf("Expect out is %q, but %q", out.String(), expected)
+		t.Fatalf("Expected '%q', but got '%q'", expected, out.String())
 	}
 
 	out.Reset()
 	measure("testdata/", retryCount, out, err)
 	expected = "test.txt: \x1b[32m9B\x1b[0m\ntest.zip: \x1b[32m8.5K\x1b[0m\n"
 	if out.String() != expected {
-		t.Fatalf("Expect out is %q, but %q", out.String(), expected)
+		t.Fatalf("Expected '%q', but got '%q'", expected, out.String())
 	}
 }
